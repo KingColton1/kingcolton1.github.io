3af9b838cbc164fc44a06e6220dea48171b7021a
kingcolton <coltonbailiff@yahoo.com>
2023-10-14 18:26:11 -0400

    complete replace of website

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# UPDATE AS OF MAY 11, 2020
-This github repository is no longer used to host website, I've moved them to AWS (Amazon Web Service) for better performance, protection, and greater control. New link can be found at: coltonbailiff.net
-
-# kingcolton1.github.io
-A personal portfilo website designed specifically for the resume. This is a open-source website for anyone to use my website design.
-
-This will be a collection of my works from my college course's project(s) and my own personal project to teach myself. My major is Mobile Application Development at Rochester Institute of Technology, specialize in developing cross-platform application using Xamarin in Visual Studio. I've self-taught how to develop an website application using ASP.NET Core (Razor and MVC) as well.
-
-# ASP.NET
-This website will be developed in ASP.NET later on, but for now; I will be developing a simple website with only HTML and CSS using Bootstrap (to beautifully stylize the website). However, I will include my ASP.NET projects in my website to show Recruiter that I have experiences with them.
+Using this repo as my personal testing ground for live website.
---
+++ fetch.js
@@ -0,0 +1,93 @@
+function insertData(id, json)
+{
+	if (arguments[1]['online'] == true) {
+		const template_online = `
+		<div class="status-header">
+			<h1 class="title"><i class="status online fa fa-circle"></i> ${arguments[1]['host']}:${arguments[1]['port']}</h1>
+			<h2 class="subtitle">${arguments[1]['motd']['clean']}</h2>
+		</div>
+
+		<div class="status-list">
+			<div class="label"><i class="fa fa-server"></i> Server stats</div>
+			<table class="table">
+				<tbody>
+				<tr>
+					<td>Version</td>
+					<td>${arguments[1]['version']['name_clean']}</td>
+				</tr>
+				<tr>
+					<td>Protocol</td>
+					<td>${arguments[1]['version']['protocol']}</td>
+				</tr>
+				</tbody>
+			</table>
+		</div>
+		<div class="status-list">
+			<div class="players">
+				<div class="label"><i class="fa fa-users"></i> Players online</div>
+				<div class="bar">
+					<progress class="progress" value="${arguments[1]['players']['online']}" max="${arguments[1]['players']['max']}"><?= $this->e($players['percentage']) ?>%</progress>
+				</div>
+				<div class="amount has-text-centered">${arguments[1]['players']['online']}/${arguments[1]['players']['max']}</div>
+			</div>
+		</div>
+		<div class="status-list">
+			<div class="online-players-list">
+				<div class="label"><i class="fa fa-list-users"></i>List of Online Players</div>
+				<div class="popup" id="popup">
+					<h2>List of Online Players</h2>
+					<p class="online-name">${arguments[1]['players']['list']['name_clean']}</p>
+				</div>
+			</div>
+		</div>
+		`;
+		var template = document.createElement('div');
+		template.className = "section";
+		template.innerHTML = template_online;
+		document.getElementById(arguments[0]).append(template);
+	} else {
+		const template_offline = `
+		<div class="status-header">
+			<h1 class="title"><i class="status offline fa fa-circle"></i> ${arguments[1]['host']}:${arguments[1]['port']}</h1>
+			<h2 class="subtitle">Not available</h2>
+		</div>
+
+		<div class="status-list">
+			<div class="label"><i class="fa fa-warning"></i> Server offline</div>
+		</div>
+		`;
+		var template = document.createElement('div');
+		template.className = "section";
+		template.innerHTML = template_offline;
+		document.getElementById(arguments[0]).append(template);
+	}
+}
+async function fetchData(ip)
+{
+	var response = await fetch('https://api.mcstatus.io/v2/status/java/' + arguments[0]);
+	var data = await response.json();
+	data = JSON.stringify(data);
+	data = JSON.parse(data);
+	return data;
+}
+async function fetchAddress()
+{
+	var response = await fetch('server.txt');
+	var response_text = response.text();
+	console.debug(response_text);
+	return response_text;
+}
+async function fetchServers()
+{
+	var txt_servers = await fetchAddress();
+	var servers = txt_servers.split(",");
+	console.debug(servers);
+
+	for (let i = 0; i < servers.length; i++) {
+		console.debug(servers[i]);
+
+		var server_json = await fetchData(servers[i]);
+		insertData('content', server_json);
+	}
+}
+fetchServers();
